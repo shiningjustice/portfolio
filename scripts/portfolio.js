e1486e68eb03182da8d9e0710210ef65919174be
Phoebe Law <phoebebasilio@gmail.com>
2019-12-21 12:01:01 -0800

    Adds basic structure/formatting

---
+++ scripts/portfolio.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const portfolio = (() => {
+
+  //Render functions
+  const render = () => {
+    console.log('render ran');
+    renderMainHtml();
+  };
+
+  const renderMainHtml = () => {
+    $('.replaceForMainHtml').html(`
+      <div class="portfolioContainer">
+        <section id="about" class="Main__section about">
+          <div class="intro">
+            <h2 class="intro">
+              <span class="g1">h</span><span class="g2">e</span><span class="g3">l</span><span class="g4">l</span><span class="g5">o</span>
+              <span class="g6"> </span>
+              <span class="g7">w</span><span class="g8">o</span><span class="g9">r</span><span class="g10">l</span><span class="g11">d</span>
+            </h2>
+            <h3 class="intro secondaryFont" id="h3Intro"><span class="notBold">I'm</span> Phoebe Law</h3>
+          </div>
+          <p class="notIntro">I'm a Junior Full-stack Javascript Developer driven by creativity and excellence. When I'm not coding or trying to describe myself I spend time with people I love, climb, or pick something to do from a myriad of hobbies. I also wear lots of deodorant. </p>        <p>
+        </section>
+        <section id="projects" class="Main__section projects notIntro">
+          <h2>Projects</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+        <section id="contact" class="Main__section contact notIntro">
+          <h2>Contact</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          <ul class="contact__ul">
+            <li></li>
+            <li><a href="https://refer.nativecos.com/x/RFpw36" title="This is embarassing but if you have BO, this is the place to go!">Deodorant</a></li>          
+          <ul>
+        </section>
+      </div>
+    `)
+  };
+
+  // Event handler and listener functions
+  const setAllHandlersAndListeners = () => {
+    onClickLightSwitch();
+    hideIntroOnlyItems();
+  };
+
+  const onClickLightSwitch = () => {
+    $('.body').on('click', '.lightSwitch', () => {
+      console.log($('.body').css('background-color'));
+      if ($('.body').css('background-color') === "rgba(0, 0, 0, 0)" || $('.body').css('background-color') === "rgb(0, 0, 0)")  {
+        console.log('was black');
+        $('.body').css({"background-color": "white", "color": "black"})
+      } else {
+        console.log('was white');
+        $('.body').css({"background-color": "#252525", "color": "whitesmoke"})
+      }
+    })
+  };
+
+  const hideIntroOnlyItems = () => {
+    setTimeout(() => {
+      const introOnlyItems = document.getElementById('h3Intro');
+
+      introOnlyItems.classList.toggle('hidden');
+    }, 7000)
+  }
+
+  // Return function
+  return {
+    render,
+    setAllHandlersAndListeners
+  };
+})();
