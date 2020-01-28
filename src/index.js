4a127e54352cd180a31b99950a7bc315f12a648c
Phoebe Law <phoebebasilio@gmail.com>
2020-01-27 18:51:48 -0800

    moves animation functions to local css files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
 import './index.css';
-import './animations.css';
 import App from './Components/App/App';
 
 ReactDOM.render(
