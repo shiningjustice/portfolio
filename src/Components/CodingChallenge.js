0ae9dc389e7f27524560bc4592654abf3cfc5aad
Phoebe Law <phoebebasilio@gmail.com>
2020-03-25 17:36:10 -0700

    Fixes date bug, adds styling to CodingChallenge

--- src/Components/CodingChallenge.js
+++ src/Components/CodingChallenge.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 const CodingChallenge = () => {
   const [currDayOfChallenge, setCurrDayOfChallenge] = useState(null);
   const dayChallengeStarted = moment('January 27, 2020');
-  const today = moment('January 28, 2020');
+  const today = moment(new Date());
   const daysSinceChallengeStarted = today.diff(dayChallengeStarted, 'days');
 
   return (
@@ -15,10 +15,10 @@ const CodingChallenge = () => {
       <h1>100 Days of Code</h1>
       <p>Challenge info:</p>
       <ul>
-        <li><span className="bold">First Day of Challenge:</span> January 27, 2020</li>
-        <li><span className="bold">Days since Challenge Started:</span> {daysSinceChallengeStarted}</li>
-        <li><span className="bold">Current Day of Challenge:</span> <input onChange={(event) => setCurrDayOfChallenge(event.target.value)}></input></li>
-        <li><span className="bold">Days behind:</span> {currDayOfChallenge ? (daysSinceChallengeStarted - currDayOfChallenge + 1) : 'Update the field above to calculate the number of days behind.'}</li>
+        <li className="text-left"><span className="bold">First Day of Challenge:</span> January 27, 2020</li>
+        <li className="text-left"><span className="bold">Days since Challenge Started:</span> {daysSinceChallengeStarted}</li>
+        <li className="text-left"><span className="bold">Current Day of Challenge:</span> <input onChange={(event) => setCurrDayOfChallenge(event.target.value)}></input></li>
+        <li className="text-left"><span className="bold">Days behind:</span> {currDayOfChallenge ? (daysSinceChallengeStarted - currDayOfChallenge + 1) : 'Update the field above to calculate the number of days behind.'}</li>
       </ul>
     </div>
   );
